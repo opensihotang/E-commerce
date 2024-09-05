5b4ddf5eab9b65972e799c3434181430f36a90b4
ISGS-OPEN\admin <opensihotang56@gmail.com>
2024-09-05 17:00:21 +0700

    add appbar and footer

---
+++ client/src/components/appbar/Appbar.jsx
@@ -0,0 +1,81 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import LoginIcon from "@mui/icons-material/Login";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { useState } from "react";
+const [open, setOpen] = useState(null);
+
+const AppsBar = () => {
+  const adminMenu = ["Setting", "Dashboard", "Logout"];
+  const userMenu = ["Profile", "Orders", "Logout"];
+  const handleOpenMenu = (event) => {
+    setOpen(event.currentTarget);
+    console.log("testt");
+  };
+  const handleCloseMenu = (event) => {
+    setOpen(null);
+  };
+  const user = "user";
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <img
+            src="logo2.png"
+            alt="logo"
+            style={{
+              height: "60px",
+              width: "120px",
+              objectFit: "contain",
+            }}
+          />
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <IconButton color="inherit">
+              <ShoppingCartOutlinedIcon />
+            </IconButton>
+            {user === "user" ? (
+              <>
+                <IconButton color="inherit">
+                  onClick={handleOpenMenu}
+                  <AccountCircleOutlinedIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={open}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(open)}
+                  onClose={handleCloseMenu}
+                >
+                  {userMenu.map((item) => {
+                    <MenuItem onClick={handleCloseMenu} key={item}>
+                      {item}
+                    </MenuItem>;
+                  })}
+                </Menu>
+              </>
+            ) : (
+              <IconButton color="inherit">
+                <LoginIcon />
+              </IconButton>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+export default AppsBar;
