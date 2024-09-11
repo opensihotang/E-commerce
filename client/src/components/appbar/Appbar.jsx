b0e5bf0da902417d1d1cc9b8e8afcec3ebacc369
ISGS-OPEN\admin <opensihotang56@gmail.com>
2024-09-11 15:58:36 +0700

    update --feat profile

--- client/src/components/appbar/Appbar.jsx
+++ client/src/components/appbar/Appbar.jsx
@@ -9,19 +9,21 @@ import LoginIcon from "@mui/icons-material/Login";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import { useState } from "react";
-const [open, setOpen] = useState(null);
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const AppsBar = () => {
+  const user = "admin";
   const adminMenu = ["Setting", "Dashboard", "Logout"];
   const userMenu = ["Profile", "Orders", "Logout"];
+
+  const [open, setOpen] = useState(null);
   const handleOpenMenu = (event) => {
     setOpen(event.currentTarget);
-    console.log("testt");
   };
   const handleCloseMenu = (event) => {
     setOpen(null);
   };
-  const user = "user";
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -41,8 +43,7 @@ const AppsBar = () => {
             </IconButton>
             {user === "user" ? (
               <>
-                <IconButton color="inherit">
-                  onClick={handleOpenMenu}
+                <IconButton color="inherit" onClick={handleOpenMenu}>
                   <AccountCircleOutlinedIcon />
                 </IconButton>
                 <Menu
@@ -60,11 +61,38 @@ const AppsBar = () => {
                   open={Boolean(open)}
                   onClose={handleCloseMenu}
                 >
-                  {userMenu.map((item) => {
+                  {userMenu.map((item) => (
                     <MenuItem onClick={handleCloseMenu} key={item}>
                       {item}
-                    </MenuItem>;
-                  })}
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            ) : user === "admin" ? (
+              <>
+                <IconButton color="inherit" onClick={handleOpenMenu}>
+                  <AccountCircleOutlinedIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={open}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(open)}
+                  onClose={handleCloseMenu}
+                >
+                  {adminMenu.map((item) => (
+                    <MenuItem onClick={handleCloseMenu} key={item}>
+                      {item}
+                    </MenuItem>
+                  ))}
                 </Menu>
               </>
             ) : (
