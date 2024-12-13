b9aabead607e135c411efc490826c34f83ef60de
ISGS-OPEN\admin <opensihotang56@gmail.com>
2024-12-13 17:15:58 +0700

    table order

--- client/src/pages/user/order/ListOrder.jsx
+++ client/src/pages/user/order/ListOrder.jsx
@@ -1,5 +1,27 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Input,
+  Paper,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import TableOrder from "./TableOrder";
 
+const columns = [
+  { id: "name", label: "Name", minWidth: 170 },
+  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+  {
+    id: "population",
+    label: "Population",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+];
 const ListOrder = () => {
   return (
     <>
@@ -10,8 +32,31 @@ const ListOrder = () => {
           padding: "20px",
         }}
       >
-        <Box>pesanan kamu</Box>
-        <Box>search</Box>
+        <Typography variant="h6" fontWeight="bold">
+          pesanan kamu
+        </Typography>
+        <Box>
+          <Input type="text" placeholder="search" />
+        </Box>
+        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+            </Table>
+          </TableContainer>
+        </Paper>
       </Box>
     </>
   );
---
+++ client/src/pages/user/order/TableOrder.jsx
@@ -0,0 +1,5 @@
+function TableOrder() {
+  return <div>TableOrder</div>;
+}
+
+export default TableOrder;
